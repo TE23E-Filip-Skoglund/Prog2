1051984802e4afd33ea56c2df41da85fed9fe630
SkogisAB <filip.skoglund@elev.ntig.se>
2026-01-26 12:57:43 +0100

    logik som testar funktionalitet

--- lek2_uppgifter/uppg1/src/App.java
+++ lek2_uppgifter/uppg1/src/App.java
@@ -1,6 +1,17 @@
 void main() {
     IO.println("============ WILKOMMEN ============");
 
-    
+    Bil bil1 = new Bil("Volvo", "245 GL", "ABC123", 1983);
+    IO.println(bil1.toString());
 
+    bil1.ökaHastighet(50);
+    IO.println("Bilens hastihget är nu: " + bil1.getHastighet());
+
+    bil1.bromsa(20);
+    IO.println("Bilens hastihget är nu: " + bil1.getHastighet());
+
+    // Detta ska ge exception
+    bil1.ökaHastighet(-10);
+
+    Bil bil2 = new Bil("", "245 GL", "ABC123", 1983);
 }
