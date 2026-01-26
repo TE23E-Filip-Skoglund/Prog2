7f09518f2a352595eb95ff37fc31a7a9a741de5c
SkogisAB <filip.skoglund@elev.ntig.se>
2026-01-26 13:14:13 +0100

    ny klass: bilregister, med en konsturktör som skapar en tom array-lista

---
+++ lek2_uppgifter/uppg2/src/BilRegister.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class BilRegister {
+
+    private ArrayList<Bil> bilLista;
+
+    public BilRegister() {
+        this.bilLista = new ArrayList<Bil>();
+    }
+}
