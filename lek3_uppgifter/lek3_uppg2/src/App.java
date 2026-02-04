062a8a56a088ffe2bf122932664eabe989e0cc9c
SkogisAB <filip.skoglund@elev.ntig.se>
2026-02-04 11:30:28 +0100

    main meny med tillhörande logik

--- lek3_uppgifter/lek3_uppg2/src/App.java
+++ lek3_uppgifter/lek3_uppg2/src/App.java
@@ -1,12 +1,46 @@
 void main() {
+    ArrayList<Vagfordon> fordon = new ArrayList<Vagfordon>();
+
+    while (true) {
+        String choice = meny();
+        switch (choice) {
+            case "1":
+
+                break;
+            case "2":
+                break;
+
+            case "3":
+
+                break;
+            case "4":
+                IO.println("Avslutar programm...");
+                System.exit(0);
+                break;
+            default:
+                throw new IllegalArgumentException("Måste ange 1-4");
+        }
+    }
 
 }
 
-void meny() {
+String meny() {
 
-    IO.println("""
-        ============= Meny =============
-        1. 
-            """);
+    while (true) {
+        IO.println("""
+                ============= Meny =============
+                1. Skapa Bil
+                2. Skapa Lastbil
+                3. Lista skapade VägFordon
+                4. Avsluta program
+                            """);
+        IO.print("\nVälj ett alternativ: ");
+        String choice = IO.readln();
 
+        if (!choice.isBlank() && choice.matches("1|2|3|4")) {
+            return choice;
+        } else {
+            continue;
+        }
+    }
 }
