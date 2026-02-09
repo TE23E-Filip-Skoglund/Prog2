caffff842531da6823e1ab465f9c24705cef2b7c
SkogisAB <filip.skoglund@elev.ntig.se>
2026-02-09 13:16:58 +0100

    skapabil + skapalastbil osv, färdigställt programmet

--- lek3_uppgifter/lek3_uppg2/src/App.java
+++ lek3_uppgifter/lek3_uppg2/src/App.java
@@ -1,17 +1,17 @@
 void main() {
-    ArrayList<Vagfordon> fordon = new ArrayList<Vagfordon>();
+    ArrayList<Vagfordon> fordonsLista = new ArrayList<Vagfordon>();
 
     while (true) {
         String choice = meny();
         switch (choice) {
             case "1":
-
+                skapaBil(fordonsLista);
                 break;
             case "2":
+                skapaLastbil(fordonsLista);
                 break;
-
             case "3":
-
+                listaFordon(fordonsLista);
                 break;
             case "4":
                 IO.println("Avslutar programm...");
@@ -38,9 +38,79 @@ String meny() {
         String choice = IO.readln();
 
         if (!choice.isBlank() && choice.matches("1|2|3|4")) {
-            return choice;
+            return choice.trim();
         } else {
             continue;
         }
     }
+}
+
+void skapaBil(ArrayList<Vagfordon> fordonsLista) {
+    String regnr = inputString("Ange regnr: ", "[A-Za-z]{3}+[0-9]{3}+");
+    String ägare = inputString("Ange ägare: ", null);
+    String färg = inputString("Ange färg: ", null);
+    int vikt = inputInt("Ange vikt: ");
+    int sittPlatser = inputInt("Ange sittPlatser: ");
+
+    Bil bil = new Bil(regnr, ägare, färg, vikt, sittPlatser);
+    fordonsLista.add(bil);
+
+    väntaPåTryck();
+}
+
+void skapaLastbil(ArrayList<Vagfordon> fordonsLista) {
+    String regnr = inputString("Ange regnr: ", "[A-Za-z]{3}+[0-9]{3}+");
+    String ägare = inputString("Ange ägare: ", null);
+    String färg = inputString("Ange färg: ", null);
+    int vikt = inputInt("Ange vikt: ");
+    int lastkapacitet = inputInt("Ange lastkapacitet: ");
+
+    Lastbil lastbil = new Lastbil(regnr, ägare, färg, vikt, lastkapacitet);
+    fordonsLista.add(lastbil);
+
+    väntaPåTryck();
+}
+
+void listaFordon(ArrayList<Vagfordon> fordonsLista) {
+    IO.println("\n\n\n\n\n=============== Lista över alla fordon ===============");
+    IO.println(fordonsLista.toString());
+
+    väntaPåTryck();
+}
+
+String inputString(String msg, String regex) {
+
+    while (true) {
+        IO.print(msg);
+        String input = IO.readln();
+
+        if (!input.isBlank()) {
+            if (regex == null || input.matches(regex)) {
+                return input;
+            } else {
+                IO.print("Fel format, följ standard formatet");
+            }
+        } else {
+            IO.print("Fel format, vg ange en String");
+        }
+    }
+}
+
+int inputInt(String msg) {
+    while (true) {
+        IO.print(msg);
+        String input = IO.readln();
+
+        try {
+            int inputInt = Integer.parseInt(input.trim());
+            return inputInt;
+        } catch (Error e) {
+            throw new IllegalArgumentException("vg ange ett heltal");
+        }
+    }
+}
+
+void väntaPåTryck() {
+    IO.println("\nTryck på Enter för att gå tillbaka till menyn...");
+    IO.readln();
 }
