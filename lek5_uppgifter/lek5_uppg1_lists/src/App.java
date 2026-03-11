39d1559c0df33c07c8145882ac44c49e809876c2
SkogisAB <filip.skoglund@elev.ntig.se>
2026-03-11 10:30:45 +0100

    printlist och main metod

--- lek5_uppgifter/lek5_uppg1_lists/src/App.java
+++ lek5_uppgifter/lek5_uppg1_lists/src/App.java
@@ -1,5 +1,18 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.ArrayList;
+import java.util.List;
+
+void main() {
+    List<Person> register = new ArrayList<>();
+
+    // 1. Lägg till person
+    register.add(new Person("Anna", 1990));
+    register.add(new Person("Berit", 1955));
+    register.add(new Person("Carl", 2005));
+    register.add(new Person("David", 1982));
+}
+
+static void printList(List<Person> register) {
+    for (Person person : register) {
+        IO.println(person.toString());
     }
 }
