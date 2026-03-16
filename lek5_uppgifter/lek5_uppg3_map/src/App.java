d72be2f208edcf87bac1690b777965e030a58145
SkogisAB <filip.skoglund@elev.ntig.se>
2026-03-16 13:38:38 +0100

    slut lektion mån

--- lek5_uppgifter/lek5_uppg3_map/src/App.java
+++ lek5_uppgifter/lek5_uppg3_map/src/App.java
@@ -4,14 +4,43 @@ import java.util.List;
 void main() {
     /* Skapar lager och lägger till alla listor */
     List<Vara> lager = new ArrayList<>();
-    
+
     lager.add(new Vara("Kaffe", 25, 5));
     lager.add(new Vara("Bulle", 20, 5));
     lager.add(new Vara("Läsk", 15, 3));
     lager.add(new Vara("Glass", 10, 3));
     lager.add(new Vara("Godispåsar", 10, 3));
 
-    IO.println(lager);
+    /* Programloop */
+    while (true) {
+        Vara vald = väljVara(lager);
+       
+        if (vald.getAntal() == 0) {
+            IO.println("Varan är slut i lager, vill du köpa in? (ja/nej)");
+            if (IO.readln().toLowerCase().trim().matches("ja|nej")) {
+                String antal = IO.readln("Ange hur många? (1-5)");
+            }
+        
+        }
+        
 
 
+    }
+
+}
+
+static Vara väljVara(List<Vara> lager) {
+    while (true) {
+        String varonamn = IO.readln("\nANGE SÅLD VARA (tom för avslut): ");
+        if (varonamn.isBlank()) {
+            IO.println("Avslutar...");
+            System.exit(0);
+        }
+        for (Vara vara : lager) {
+            if (vara.getNamn().matches(varonamn)) {
+                return vara;
+            }
+        }
+        IO.println("Varan fanns inte, vg försök igen");
+    }
 }
