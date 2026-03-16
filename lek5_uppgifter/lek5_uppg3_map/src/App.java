1eb1cb89a0327ee927d38d3fc8e38396c213999b
SkogisAB <filip.skoglund@elev.ntig.se>
2026-03-16 13:24:00 +0100

    skapa lager och lägg till saker

--- lek5_uppgifter/lek5_uppg3_map/src/App.java
+++ lek5_uppgifter/lek5_uppg3_map/src/App.java
@@ -1,5 +1,17 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+import java.util.ArrayList;
+import java.util.List;
+
+void main() {
+    /* Skapar lager och lägger till alla listor */
+    List<Vara> lager = new ArrayList<>();
+    
+    lager.add(new Vara("Kaffe", 25, 5));
+    lager.add(new Vara("Bulle", 20, 5));
+    lager.add(new Vara("Läsk", 15, 3));
+    lager.add(new Vara("Glass", 10, 3));
+    lager.add(new Vara("Godispåsar", 10, 3));
+
+    IO.println(lager);
+
+
 }
